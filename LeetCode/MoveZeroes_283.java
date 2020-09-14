7f97761cec40974597a0f9398fdbce7dc38c8f38
kangzhe <1274789595@qq.com>
2020-09-14 21:24:37 +0800

    Create MoveZeroes_283.java

---
+++ LeetCode/MoveZeroes_283.java
@@ -0,0 +1,37 @@
+package com.isoftstone.file.vacamanager.ayuan;
+
+/**
+ * @author yuanyuan
+ * @Desc 283. 移动零（双指针）
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ * <p>
+ * https://leetcode-cn.com/problems/move-zeroes/
+ * @Date 2020/9/9 16:02
+ */
+public class MoveZeroes_283 {
+
+    /**
+     * 这里参考了快速排序的思想，快速排序首先要确定一个待分割的元素做中间点x，然后把所有小于等于x的元素放到x的左边，大于x的元素放到其右边。
+     * 这里我们可以用0当做这个中间点，把不等于0(注意题目没说不能有负数)的放到中间点的左边，等于0的放到其右边。
+     * 这的中间点就是0本身，所以实现起来比快速排序简单很多，我们使用两个指针i和j，只要nums[i]!=0，我们就交换nums[i]和nums[j]
+     * <p>
+     * 链接：https://leetcode-cn.com/problems/move-zeroes/solution/dong-hua-yan-shi-283yi-dong-ling-by-wang_ni_ma/
+     *
+     * @param nums
+     */
+    public void moveZeroes(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            ////当前元素!=0，就把其交换到左边，等于0的交换到右边
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
+    }
+}
